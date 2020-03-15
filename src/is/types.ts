6f0ef703fc837ce713828aa62b78c2fc9b40f584
shuaiquan <994011557@qq.com>
2020-03-15 13:41:28 +0800

    添加基本类型的判断

---
+++ src/is/types.ts
@@ -0,0 +1,38 @@
+/**
+ * 基本类型的判断
+ */
+
+export function isNumber(value: any) {
+    return typeof value === 'number';
+}
+
+export function isString(value: any) {
+    return typeof value === 'string';
+}
+
+export function isBoolean(value: any) {
+    return typeof value === 'boolean';
+}
+
+export function isUndefined(value: any) {
+    return value === undefined;
+}
+
+export function isNull(value: any) {
+    return value === null;
+}
+
+export function isFunc(value: any) {
+    return typeof value === 'function';
+}
+
+export function isObject(value: any) {
+    return typeof value === 'object' && value !== null;
+}
+
+export function isArray(value: any) {
+    /**
+     * 另新语法：'isArray' in Array
+     */
+    return isFunc(Array.isArray) ? Array.isArray(value) : Object.prototype.toString.call(value) === '[object Array]';
+}
